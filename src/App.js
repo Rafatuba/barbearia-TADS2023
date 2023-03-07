7fd2e31f51e37f6664e34c46f56246d7bc2327cf
Rafatuba <rafaolly1@gmail.com>
2023-03-07 15:30:37 -0400

    página login

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import { useState } from 'react';
+import logoBarber from './assets/barber-logo-2.png';
+import './style.css';
+import Zoom from 'react-reveal/Zoom';
 
 function App() {
+
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="container-login">
+      <Zoom bottom>
+        <div className="wrap-login">
+          <form className="login-form">
+
+            <span className="login-form-title">Bem Vindo!</span>
+            
+            <span className="login-form-title">
+              <img src={logoBarber} alt="Logo Barbearia" />
+            </span>
+          
+            <div className="wrap-input">
+              <input
+               className={email !== "" ? 'has-val input' : 'input'} 
+               type="email"
+               value={email}
+               onChange={e => setEmail(e.target.value)}
+              />
+              <span className="focus-input" data-placeholder='Email'></span>
+            </div>
+
+            <div className="wrap-input">
+              <input 
+                className={password !== "" ? 'has-val input' : 'input'}
+                type="password" 
+                value={password}
+               onChange={e => setPassword(e.target.value)}
+              />
+              <span className="focus-input" data-placeholder='Password'></span>
+            </div>
+
+            <div className="container-login-form-btn">
+              <button className='login-form-btn'>Login</button>
+            </div>
+
+            <div className="text-center">
+              <span className="txt1">Esqueci a senha</span>
+              <a href="#" className="txt2">Recuperar Senha</a>
+            </div>
+
+          </form>
+        </div>
+      </Zoom>
+      </div>
     </div>
   );
 }
